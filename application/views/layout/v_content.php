e8ffba03459804aca7346b73e9b3fb743715e383
nita dewi <nitadewis@gmail.com>
2020-12-17 11:07:52 +0700

    templating

---
+++ application/views/layout/v_content.php
@@ -0,0 +1,29 @@
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+<!-- Page Heading -->
+<h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+
+
+<div class="card shadow mb-4">
+                                <div class="card-header py-3">
+                                    <h6 class="m-0 font-weight-bold text-primary">Basic Card Example</h6>
+                                </div>
+                                <div class="card-body">
+                                <?php
+                                        if ($isi){
+                                            $this->load->view($isi);
+                                        }
+                                        ?>
+                                </div>
+                            </div>
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
+
+
